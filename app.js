015b4f98ed925320d8d64b45d21bbbc402aba6ca
Fran1999s <franqoorodriguez@gmail.com>
2022-12-14 23:58:41 -0300

    Agregue el start en el Package.json

--- app.js
+++ app.js
@@ -2,12 +2,14 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const port = 3030;
+const port = 3000;
 
 app.use (express.static("public"));
 app.get ("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/home.html"))
 });
 
+
+const port = process.env.PORT || 3000;
 app.listen (port, () => 
 console.log("Esta corriendo el puerto " + port));
